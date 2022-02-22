bd798ef5e0ad62cb33b3f446c1642738e209bf32
Mel <limelanie01@gmail.com>
2022-02-21 23:45:40 -0500

    week2- poem generator

---
+++ week2/script.js
@@ -0,0 +1,40 @@
+const myTitle = 'Way of Life'
+let verb = ['Driving', 'Striving', 'Competing', 'Craving', 'Thriving', 'Creating', 'Completing']
+let content =['the road of', 'throughout my', 'against others in', 'for food in', 'for the sake of', 'fun in', 'the goal in']
+let finalword = ['life.']
+let space = ['&nbsp',
+'&nbsp'+'&nbsp',
+'&nbsp'+'&nbsp'+'&nbsp',
+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp',
+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp',
+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp',
+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp', 
+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp'+'&nbsp']
+
+function returnPoem(title, verb, content, finalword) {
+  return title + ': ' + verb + ' ' + content + ' ' + finalword
+}
+
+function forLoopPoem(title, verb, content, finalword) {
+  W('Original Poem:')
+  W(title + ' by Melanie Li')
+  for (i = 0; i < 7; i++) {
+    W(verb[i] + ' ' + content[i] + ' ' + finalword[0])
+  }
+  W('')
+}
+
+forLoopPoem(myTitle, verb, content, finalword)
+
+// while loop poem: Random Loop
+let count = 0
+W('Poem Generator:')
+while(count < 7) {
+  W(getRandomItem(space) + getRandomItem(verb) + getRandomItem(space) + getRandomItem(content) + getRandomItem(space) + getRandomItem(finalword))
+  count++
+}
+
+function getRandomItem(array) {
+  //selects a random item from the array
+return array[Math.floor(Math.random()*array.length)]
+}
